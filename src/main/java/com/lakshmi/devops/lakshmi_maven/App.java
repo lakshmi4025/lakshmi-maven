a00eed061d4d54341e61fb819f0cefa2b98cb9cd
lakshmi <laksmi.pokuru1994@gmail.com>
2021-08-20 04:12:55 -0700

    checkin for maven project

---
+++ src/main/java/com/lakshmi/devops/lakshmi_maven/App.java
@@ -0,0 +1,13 @@
+package com.lakshmi.devops.lakshmi_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from lakshmi!" );
+    }
+}
